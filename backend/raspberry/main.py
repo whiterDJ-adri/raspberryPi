ca63da1d577597fd41545ef0184be22451126642
Lucia <luciaabellan6@gmail.com>
2025-10-05 19:31:05 +0200

    Update rasberry movimiento ok

--- backend/raspberry/main.py
+++ backend/raspberry/main.py
@@ -1,25 +1,56 @@
 import os, cv2, requests
 from datetime import datetime
+import numpy as np
 
-def take_photo():
-    # Inicia la camara y toma una foto
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    cap.release()
+def take_photo(frame):
     
     # Definimos la ruta y el nombre del fichero 
     ruta = "screenshots"
     nombre_fichero = datetime.now().strftime('%Y%m%d_%H%M%S') + ".jpg";
     ruta_fichero = os.path.join("../api/media", ruta, nombre_fichero)
     
-    # Guardamos la imagen
-    cv2.imwrite(ruta_fichero, frame)
+    # Formatea el frame2 para que sea jpg y luego se crea el fichero con su nombre, se pasa a binario y le indicamos el tipo que es, en este caso jpeg
+    res, buffer = cv2.imencode(".jpg", frame2)
+    print(res)
+    file = { "file": (nombre_fichero, buffer.tobytes(), "image/jpeg")}
     
     insert = {
-        "name": nombre_fichero,
-        "date": datetime.now().strftime('%Y%m%d_%H%M%S'),
-        "path_file": ruta_fichero
+        "fichero": nombre_fichero,
+        "fecha": datetime.now().strftime('%Y%m%d_%H%M%S'),
+        "ruta": ruta_fichero,
+        "binario": file
     }
-    return requests.post("http://localhost:5000/api/photo", json=insert)
+    return 
+    # return requests.post("http://localhost:5000/api/photo", json=insert)
 
-take_photo()
+
+# Abrimos la cámara (0 = cámara principal)
+cap = cv2.VideoCapture(0)
+
+ret, frame1 = cap.read()
+
+while True:
+    # Leemos el siguiente frame
+    ret, frame2 = cap.read()
+    if not ret:
+        break
+
+    # Calculamos la diferencia entre las dos imagenes
+    diferencia = cv2.absdiff(frame1, frame2)
+
+    # Convertimos la diferencia a escala de grises (para analizarla mejor)
+    gris = cv2.cvtColor(diferencia, cv2.COLOR_BGR2GRAY)
+
+    # Calculamos el valor medio de la diferencia (cuanto a cambiado la iomagen)
+    promedio = np.mean(gris)
+
+
+    # Si el promedio supera un valor, consideramos que hay movimiento
+    if promedio > 10:  # puedes ajustar este número según lo sensible que quieras
+        print("¡Movimiento detectado! Valor:", promedio)
+        take_photo(frame2)
+    else:
+        print("Sin movimiento... Valor:", promedio)
+
+    # Actualizamos el frame anterior
+    frame1 = frame2
