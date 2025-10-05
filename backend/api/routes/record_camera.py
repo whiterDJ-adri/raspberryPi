a4ee6cddeabd108b8ce3ff18f66bad29cd12ff62
Lucia <luciaabellan6@gmail.com>
2025-10-05 20:31:38 +0200

    update

--- backend/api/routes/record_camera.py
+++ backend/api/routes/record_camera.py
@@ -33,7 +33,7 @@ def add_foto():
     }
     
     data_file = request.files["file"]
-    data_file.save(f"../media/screenshots/{data_db['fichero']}")
+    data_file.save(f"media/screenshots/{data_db['fichero']}")
     
     validated_data = record_camera_schema.load(data_db)
     db = get_db_controller()
