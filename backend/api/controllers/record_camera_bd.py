b427ced33a85cad1477537253aea92096d2f8352
Jhonny Claure <claurejhonny188@gmail.com>
2025-10-05 18:35:56 +0200

    update

--- backend/api/controllers/record_camera_bd.py
+++ backend/api/controllers/record_camera_bd.py
@@ -5,14 +5,21 @@ class RecordCameraController:
     def __init__(self, mongo):
         self.collection = mongo.db["record_camera"]
     
-    def get_one_route(self):
-        records = self.collection.find().limit(1)
-        return records
+    def get_one_photo(self, photo_id):
+        record = self.collection.find({"_id": ObjectId(photo_id)})
+        return record, 200
     
     def get_all_photos(self):
         records = self.collection.find()
-        return records
+        return records, 200
     
     def add_photo(self, data):
         self.collection.insert_one(data)
-        return {"msg": "Photo record created"}
+        return {"msg": "Photo record created"}, 201
+
+    def delete_photo(self, photo_id):
+        result = self.collection.delete_one({"_id": ObjectId(photo_id)})
+        if result.deleted_count == 1:
+            return {"msg": "Photo record deleted"}, 200
+        else:
+            return {"msg": "Photo record not found"}, 404
--- backend/api/routes/record_camera.py
+++ backend/api/routes/record_camera.py
@@ -13,6 +13,15 @@ def get_db_controller():
 def get_camera_controller():
     return CameraController()
 
+def get_record_controller():
+    return RecordCameraController(current_app.mongo)
+
+@record_cam_bp.route('/', methods=['GET'])
+def obtener_foto():
+    rc = get_record_controller()
+    record = rc.get_all_photos()
+    return jsonify(record), record[1]
+    
 @record_cam_bp.route('/', methods=['POST'])
 def hacer_una_foto():
     # Se llama al controlador de la camara, se usa el metodo hacer_foto y el resultado que devuelve se guarda en data
@@ -27,4 +36,16 @@ def hacer_una_foto():
     result = db.add_photo(validated_data)
     
     # Devuelve o que da el metodo add_photo
-    return jsonify(result), 201
+    return jsonify(result), result[1]
+
+@record_cam_bp.route('/<photo_id>', methods=['GET'])
+def obtener_una_foto(photo_id):
+    db = get_db_controller()
+    record = db.get_one_photo()
+    return jsonify(record), record[1]
+
+@record_cam_bp.route('/<photo_id>', methods=['DELETE'])
+def borrar_foto(photo_id):
+    db = get_db_controller()
+    result = db.delete_photo(photo_id)
+    return jsonify(result), result[1]
