e47ca9be862a72693c64ed4b09b3059a0e6b6eb7
luabde <luciaabellan6@gmail.com>
2025-10-09 10:28:24 +0200

    update

--- backend/api/static/script.js
+++ backend/api/static/script.js
@@ -2,7 +2,9 @@ async function pedir_fotos() {
     let gallery = document.getElementById("galery");
     const api = '/api/photo';
     try {
+        // Pedimos todas las fotos a la api a traves del fetch (GET)
         const response = await fetch(api);
+        // Transformamos el objeto que nos devuelve a json
         const data = await response.json();
 
         console.log(data);
