65599647dbbb4190ceaeb3139181d9da88414bb0
luabde <luciaabellan6@gmail.com>
2025-10-09 10:54:00 +0200

    update templates

--- backend/api/static/script.js
+++ backend/api/static/script.js
@@ -1,13 +1,24 @@
 async function pedir_fotos() {
-    let gallery = document.getElementById("galery");
+    let gallery = document.getElementById("gallery");
     const api = '/api/photo';
     try {
         // Pedimos todas las fotos a la api a traves del fetch (GET)
         const response = await fetch(api);
         // Transformamos el objeto que nos devuelve a json
         const data = await response.json();
+        console.log("Data: ", data);
+        let rutas = [];
+        for(let reg of data[0]){
+            let ruta = `media/screenshots/${reg.filename}`;
+            rutas.push(ruta);
+        }
 
+        console.log("Rutas: ", rutas);
         console.log(data);
+
+        rutas.forEach(r => {
+            gallery.innerHTML += `<img src="${r}">`;
+        });
     } catch(err) {
         console.error("Error: ", err);
     }
