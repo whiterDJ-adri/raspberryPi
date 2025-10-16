5a7ad5439ca7a9c5775fe9de42f7b927bd78b4d4
Jhonny Claure <claurejhonny188@gmail.com>
2025-10-16 21:47:28 +0200

    resolve problem discord

--- backend/api/services/missatge_discord.py
+++ backend/api/services/missatge_discord.py
@@ -1,27 +1,20 @@
 import os
 from discord_webhook import DiscordWebhook
 from datetime import datetime
-from zoneinfo import ZoneInfo
+import pytz 
 
 
 def send_message(data):
-    
-    # Parsear la fecha que llega en data['date']
-    
-    # Ejemplo: si tu 'date' viene como string ISO "2025-10-09T15:30:00"
+    zona_madrid = pytz.timezone("Europe/Madrid")
     data_time_formateada = datetime.fromisoformat(data["date"])
 
-    
-    # Convertir a hora de Madrid
-    hora_madrid = data_time_formateada.astimezone(ZoneInfo("Europe/Madrid"))
+    hora_madrid = data_time_formateada.astimezone(zona_madrid)
     hora_formateada = hora_madrid.strftime("%d/%m/%Y %H:%M:%S")
-    
+
     webhook = DiscordWebhook(
         url=os.getenv("WEBHOOK_DISCORD"),
         content=f"¡Nueva foto capturada! \n Hora: {hora_formateada} Filename: {data['filename']}",
     )
-
-
 
     resp = webhook.execute()
     status = getattr(resp, "status_code", 200)
